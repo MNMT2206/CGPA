b83ca291920e927e4ce9a11ad598060683940b5a
MNMT2206 <112928123+MNMT2206@users.noreply.github.com>
2024-07-18 15:58:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById("calculate-btn").addEventListener("click", function () {
     const gpas = [];
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= 8; i++) {
         const gpa = parseFloat(document.getElementById("semester" + i).value);
         if (!isNaN(gpa)) {
             gpas.push(gpa);
         }
     }
     
-    if (gpas.length === 6) {
-        const credits = [19, 18, 22, 23, 23,19];  // Credits for each semester
+    if (gpas.length === 8) {
+        const credits = [19, 18, 22, 24, 23,19,21,14];  // Credits for each semester
         const totalQualityPoints = gpas.reduce((sum, gpa, index) => sum + gpa * credits[index], 0);
         const totalCreditsSum = credits.reduce((sum, credit) => sum + credit, 0);
         const cgpa = totalQualityPoints / totalCreditsSum;
